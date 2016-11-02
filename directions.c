e82e154fc3ee31c0d11738ffd67f20574b16c0cd
Alvin Zhu <alvinzhu33@yahoo.com>
2016-11-02 18:53:38 -0400

    gooo

--- directions.c
+++ directions.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
 //Thanks to stackoverflow for these colors!!!
 #define GRN "\x1B[32m"
 #define RESET "\x1B[0m"
+#define GRY "\x1B[36m"
 
 
 int main(){
@@ -15,13 +17,20 @@ int main(){
 
     int size;
     struct dirent *file;
+    char directories[256]="";
+    char files[256]="";
     while(file = readdir(stream)){
         if(file->d_type==4){
-            printf(GRN "%s [REPO]" RESET, file->d_name);
+            strcat(directories, "\t");
+            strcat(directories, file->d_name);
+            strcat(directories, "\n");
+            //printf(GRN "%s [DIRECTORY]" RESET, file->d_name);
         }else{
-            printf("%s ", file->d_name);
+            strcat(files, "\t");
+            strcat(files, file->d_name);
+            strcat(files, "\n");
+            //printf("%s ", file->d_name);
         }
-        printf("\n");
 
         struct stat info;
         stat(file->d_name, &info);
@@ -29,8 +38,8 @@ int main(){
         //printf("%d\n", bytes);
         size += info.st_size;
     }
-    printf("\nTotal Size: ");
 
+    printf(GRY "Total Size: " RESET);
     if(size >= 1000000000){
         printf("%d.%d GB\n", size/1000000000, size%1000000000/1000000);
     }else{
@@ -45,6 +54,11 @@ int main(){
         }
     }
     printf("\n");
+
+    printf(GRY "Directories: \n" RESET);
+    printf(GRN "%s" RESET, directories);
+    printf(GRY "\nFiles: \n" RESET);
+    printf("%s\n", files);
 
     return 0;
 }
